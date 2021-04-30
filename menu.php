d4ea6b4e4b1835fd09f9ede3bcabe22f20743f9d
Péter Simon <sipec89@gmail.com>
2021-04-30 19:14:10 +0000

    Upload New File

---
+++ menu.php
@@ -0,0 +1,14 @@
+		<link rel="stylesheet" type="text/css" href="stilus.css" />
+<div class="menu">
+
+    <div id="menu_header">
+
+    <ul>
+				<li><a href="allaskeres.php">Főoldal</a></li>
+				<li><a href="2.php">2</a></li>
+				<li><a href="3.php">3</a></li>
+				<li><a href="4.php">4</a></li>
+				<li><a href="5.php">5</a></li>
+    </ul>
+   </div>
+</div>
