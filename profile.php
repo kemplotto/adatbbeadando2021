d469dda90b0296f21f7c22153ac3f1c333c79cf4
kemplotto <kemplotto@gmail.com>
2021-05-04 16:23:36 +0200

    meg nem megy
    profil adat update

--- profile.php
+++ profile.php
@@ -49,7 +49,22 @@ $stid = oci_parse($conn, "select * from USER_PROFILE where email=:email");
 					echo "Last name:"; echo $plastname; echo"<br>";
 					echo "First name:"; echo $pfirstname; echo"<br>";
 					echo "Telefonszám: "; echo $pcontact;  echo"<br>";
+	if(isset($_POST['submit'])){				
+$update = "update USER_PROFILE SET FIRST_NAME = :firstname WHERE EMAIL=:email";
+$stmt = oci_parse($conn, $update);
+oci_bind_by_name($stmt, ':firstname', $firstname);
+oci_bind_by_name($stmt, ':email', $email);
+$result = oci_execute($stmt, OCI_DEFAULT);
+if (!$result) {
+  echo oci_error();   
+}
+	}
 ?>
+
+<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+	<input type="text" name="firstname" value="" placeholder="firstname">
+	<button type="submit" name="submit">Submit</button>
+</form>
 
 </div>
 </main>
