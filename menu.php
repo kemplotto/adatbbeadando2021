7dbbdc9018bc7a90c8ab4dd3e7b4f7c748942277
Hoppnotic <83174494+Hoppnotic@users.noreply.github.com>
2021-05-06 20:27:00 +0200

    company_profil törlés
    A company profil törlése, meg majd a módosítását is ide rakom

--- menu.php
+++ menu.php
@@ -10,7 +10,7 @@
 				<li><a href="2.php">2</a></li>
 				<li><a href="3.php">3</a></li>
 				<li><a href="Company_add.php">Cég hozzáadása</a></li>
-				<li><a href="5.php">5</a></li>
+				<li><a href="5.php">Cég módosítása</a></li>
 <?php
   if (isset($_SESSION['loggedin'])){ 
       echo '<li><a href="logout.php">Kijelentkezés</a></li>';
